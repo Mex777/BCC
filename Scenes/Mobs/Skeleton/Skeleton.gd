7a579d702a8863876276543391a70717220c7a95
Mex777 <fologeaalexandru@gmail.com>
2024-05-17 10:54:41 +0300

    Fixed skeleton damage bug

--- Scenes/Mobs/Skeleton/Skeleton.gd
+++ Scenes/Mobs/Skeleton/Skeleton.gd
@@ -1,19 +1,20 @@
 extends "res://Scenes/Reusables/PatrolEnemy/PatrolEnemy.gd"
 
+var player
+
 func _physics_process(delta):
 	super(delta)
 	if attacking and cooldown_base_attack == false and not stunned:
 		attack()
-		Player.take_damage(damage)
+		player.take_damage(damage)
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Aurora":
 		attacking = true
-		if cooldown_base_attack == false and not stunned:
-			attack()
-			Player.take_damage(damage)
+		self.player = body
 
 func _on_area_2d_body_exited(body):
 	if body.name == "Aurora":
 		attacking = false
+		player = null
 
