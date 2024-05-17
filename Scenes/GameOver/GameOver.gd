23518606b487bdc9bbfb6fdf510c14b694c57c85
Mex777 <fologeaalexandru@gmail.com>
2024-05-17 20:55:37 +0300

    Added levels and transitions between them

--- Scenes/GameOver/GameOver.gd
+++ Scenes/GameOver/GameOver.gd
@@ -1,14 +1,19 @@
 extends CanvasLayer
 
+var respawn = false
+
 func _process(_delta):
-	if Player.is_dead() == true:
+	if Player.is_dead() == true and not respawn:
 		self.show()
 	else:
 		self.hide()
 
 func _on_respawn_pressed():
+	respawn = true
+	TransitionScene.transition("Chapter one")
+	await TransitionScene.on_transition_finished
 	Player.reset()
-	get_tree().change_scene_to_file("res://Scenes/World/World.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/Level1.tscn")
 
 
 func _on_main_screen_pressed():
---
+++ Scenes/Reusables/Transition/TransitionScene.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+signal on_transition_finished
+
+@onready var color_rect = $ColorRect
+@onready var animation_player = $AnimationPlayer
+@onready var text = $RichTextLabel
+
+func _ready():
+	color_rect.visible = false
+	text.visible = false
+	
+func transition(message):
+	color_rect.visible = true
+	text.visible = true
+	text.text = message
+	animation_player.play("FadeIn")
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "FadeIn":
+		on_transition_finished.emit()
+		animation_player.play("FadeOut")
+	elif anim_name == "FadeOut":
+		color_rect.visible = false
+		text.visible = false
