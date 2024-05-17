a42abbf780f287a0e26575733e21c508426e7f6e
Mex777 <fologeaalexandru@gmail.com>
2024-05-17 12:21:44 +0300

    Implemented the chase enemy

--- Scenes/Reusables/AbstractEnemy/AbstractEnemy.gd
+++ Scenes/Reusables/AbstractEnemy/AbstractEnemy.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 @export var health: float = 10
-@export var speed: float = 150.0
+@export var max_speed: float = 40.0
+var speed
 @export var damage: float = 5
 @export var attack_cooldown: float = 1
 var attacking: bool = false
@@ -9,6 +10,9 @@ var stunned: bool = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facing_right = true
 var cooldown_base_attack: bool = false
+
+func _ready():
+	speed = max_speed
 
 func take_damage(damage: int):
 	health = max(health - damage, 0)
@@ -28,12 +32,16 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	velocity.x = speed
-	if not stunned:
-		get_node("AnimatedSprite").play("Running")
+	if facing_right:
+		velocity.x = abs(speed)
 	else:
+		velocity.x = abs(speed) * -1
+		
+	if not stunned and not cooldown_base_attack:
+		$AnimatedSprite.play("Running")
+	elif not cooldown_base_attack:
 		velocity.x = 0
-		get_node("AnimatedSprite").play("Idle")
+		$AnimatedSprite.play("Idle")
 		
 	$BaseAttackTimer.wait_time = attack_cooldown
 		
@@ -42,15 +50,12 @@ func _physics_process(delta):
 func attack():
 	if not cooldown_base_attack:
 		cooldown_base_attack = true
+		$AnimatedSprite.play("Attacking")
 		$BaseAttackTimer.start()
 
 func flip():
 	facing_right = !facing_right
 	$AnimatedSprite.scale.x *= -1;
-	if facing_right:
-		speed = abs(speed)
-	else:
-		speed = abs(speed) * -1
 
 func _on_base_attack_timer_timeout():
 	cooldown_base_attack = false
