615f004c9503d8c7d0f7b60286716b21efa660c7
Vulpoi Gabriel <gvulpoi@yahoo.com>
2024-05-28 16:56:57 +0300

    Solved bug in cut scene + added return types to functions + solved indentation aesthetic problem!

--- Scenes/PauseMenu/PauseMenu.gd
+++ Scenes/PauseMenu/PauseMenu.gd
@@ -1,26 +1,30 @@
 extends CanvasLayer
 class_name PauseMenu
 
-func _input(event):
-	if Input.is_action_just_pressed("pause"):
+func _input(event) -> void:
+	if Input.is_action_just_pressed("pause") and !Game.is_in_cutscene():
 		if get_tree().paused:
 			_on_resume_button_pressed()
 		else:
 			_on_pause_button_pressed()
 
-func _on_resume_button_pressed():
+
+func _on_resume_button_pressed() -> void:
 	self.hide()
 	get_tree().paused = false
 
-func _on_pause_button_pressed():
+
+func _on_pause_button_pressed() -> void:
 	self.show()
 	get_tree().paused = true
-	
-func _on_restart_level_pressed():
+
+
+func _on_restart_level_pressed() -> void:
 	self.hide()
 	get_tree().paused = false
 	get_tree().reload_current_scene()
-		
+
+
 func _on_main_screen_pressed() -> void:
 	self.hide()
 	get_tree().paused = false
