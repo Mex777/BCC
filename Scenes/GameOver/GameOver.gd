8a957f0c0cf3f04c5d03c7a1ab3a8f29b7ccef2b
Sanda Marian-Tiberiu <tiberiusanda@gmail.com>
2024-06-10 22:00:52 +0300

    Fixed GameOver Music.

--- Scenes/GameOver/GameOver.gd
+++ Scenes/GameOver/GameOver.gd
@@ -3,16 +3,20 @@ class_name GameOver
 
 # Variable for tracking if the player has chosen to respawn.
 var respawn: bool = false
+var music_playing = false
+var audio_player: AudioStreamPlayer
 
-var audio_player: AudioStreamPlayer = AudioStreamPlayer.new()
-
-func _process(_delta: float) -> void:
+func _ready():
+	audio_player = AudioStreamPlayer.new()
+	audio_player.stream = load("res://Assets/Audio/Music/Lady of Shadows.mp3")
 	if not audio_player.is_inside_tree():
 		add_child(audio_player)
-	
+
+func _process(_delta: float) -> void:
 	if Player.is_dead() == true and not respawn:
-		audio_player.stream = load("res://Assets/Audio/Music/Lady of Shadows.mp3")
-		audio_player.play()
+		if not music_playing:
+			music_playing = true
+			audio_player.play()
 		self.show()
 	else:
 		self.hide()
@@ -20,18 +24,21 @@ func _process(_delta: float) -> void:
 
 func _on_respawn_pressed() -> void:
 	respawn = true
+	
+	music_playing = false
+	audio_player.stop()
 
 	# Transition to the first level.
 	TransitionScene.transition("Chapter one")
 	await TransitionScene.on_transition_finished
 
 	# Reset the player and game.
-	audio_player.stop()
 	Player.reset()
 	Game.reset()
 	get_tree().change_scene_to_file("res://Scenes/Levels/Level1.tscn")
 
 
 func _on_main_screen_pressed() -> void:
+	music_playing = false
 	audio_player.stop()
 	get_tree().change_scene_to_file("res://Scenes/MainMenu/MainMenu.tscn")
