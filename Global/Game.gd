766a11009be60da688728c0b29ba26e708478808
Mex777 <fologeaalexandru@gmail.com>
2025-05-14 21:48:09 +0300

    Fixed maps

--- Global/Game.gd
+++ Global/Game.gd
@@ -43,6 +43,8 @@ func advance_to_level(level_name: String) -> void:
 	TransitionScene.transition(chapter_names[level_name])
 	await TransitionScene.on_transition_finished
 	
+	SaveManager.save_game()
+	
 	# Changes the scene to the new level
 	var location: String = "res://Scenes/Levels/" + level_name + ".tscn"
 	get_tree().change_scene_to_file(location)
